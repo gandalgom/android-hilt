f924cd65a5d4ce6ed88a76a6a19451edfeeb640f
Max Yoo <devbear74@gmail.com>
2023-04-06 10:35:40 +0900

    Hilt in your Application class

--- app/src/main/java/com/gandalgom/sample/hilt/LogApplication.kt
+++ app/src/main/java/com/gandalgom/sample/hilt/LogApplication.kt
@@ -1,8 +1,11 @@
 package com.gandalgom.sample.hilt
 
 import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
 import com.gandalgom.sample.hilt.service.ServiceLocator
 
+@HiltAndroidApp
 class LogApplication : Application() {
 
     lateinit var serviceLocator: ServiceLocator
